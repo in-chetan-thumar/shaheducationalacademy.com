944d5e42654eb45a80fc593f6142eae55a6dacf4
Dev2BinDev <dev2.binarydevelopers@gmail.com>
2023-06-28 11:19:42 +0530

    Updated

--- www/app/Http/Controllers/Admin/EmailTemplateController.php
+++ www/app/Http/Controllers/Admin/EmailTemplateController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\EmailTemplateRequest;
-use App\Mail\SendVideoInviteLinkMail;
 use Illuminate\Http\Request;
 use Spatie\MailTemplates\Models\MailTemplate;
 
